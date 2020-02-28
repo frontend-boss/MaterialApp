ae2f20b4e6e8f8d51fd7312b0a00e8091a61378c
MyNameIsRene <rene.puskas@googlemail.com>
2020-02-28 14:55:55 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.less'],
+  
+  animations: [
+    trigger('openClose', [
+      state('open', style({height: '200px',})),
+      state('closed', style({height: '0px',})),
+      transition('open => closed', [animate('0.3s ease-out')]),
+      transition('closed => open', [animate('0.3s ease-out')]),
+    ]),
+
+    trigger('openCloseBackdrop', [
+      state('open', style({visibility: 'visible', cursor: 'pointer', opacity: '1'})),
+      state('closed', style({visibility: 'hidden', cursor: 'auto', opacity: '0'})),
+      transition('open => closed', [animate('0.3s ease-out')]),
+      transition('closed => open', [animate('0.3s ease-out')]),
+    ]),
+  ]
+
 })
+
 export class AppComponent {
   title = 'nextlevel-app';
+
+  isOpen = false;
+
+  toggleNav() {
+    this.isOpen = !this.isOpen;
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
